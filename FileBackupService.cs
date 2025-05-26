b7c6d1ccf795adf887abc815495e276daae1cf51
Vishal Navnath Waghmode <72240870+vish390@users.noreply.github.com>
2025-05-26 21:54:44 +0530

    Add files via upload

---
+++ FileBackupService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace FileBackupService
+{
+    internal class FileBackupService : ServiceBase
+    {
+        Timer timer;
+        string sourceDirectory = @"C:\Users\vishal waghmode\OneDrive\Desktop\Source";  // Must be a folder
+        string backupDirectory = @"C:\Users\vishal waghmode\OneDrive\Desktop\Backup";  // Must be a folder
+
+        public FileBackupService()
+        {
+            ServiceName = "FileBackupService";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Directory.CreateDirectory(sourceDirectory);
+            Directory.CreateDirectory(backupDirectory);
+            Directory.CreateDirectory(@"C:\temp");
+
+            File.AppendAllText(@"C:\temp\log.txt", $"Service started at {DateTime.Now}{Environment.NewLine}");
+
+            timer = new Timer(60000); // Run every 60 seconds
+            timer.Elapsed += OnElapsedTime;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        private void OnElapsedTime(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                foreach (var file in Directory.GetFiles(sourceDirectory))
+                {
+                    var fileName = Path.GetFileName(file);
+                    var destFile = Path.Combine(backupDirectory, fileName);
+                    File.Copy(file, destFile, true);
+                }
+
+                File.AppendAllText(@"C:\temp\log.txt", $"Backup performed at {DateTime.Now}{Environment.NewLine}");
+            }
+            catch (Exception ex)
+            {
+                File.AppendAllText(@"C:\temp\log.txt", $"Error: {ex.Message}{Environment.NewLine}");
+            }
+        }
+
+        protected override void OnStop()
+        {
+            timer.Stop();
+            File.AppendAllText(@"C:\temp\log.txt", $"Service stopped at {DateTime.Now}{Environment.NewLine}");
+        }
+    }
+}
---
+++ ProjectInstaller.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ServiceProcess;
+using System.Configuration.Install;
+
+[RunInstaller(true)]
+public class ProjectInstaller : Installer
+{
+    public ProjectInstaller()
+    {
+        ServiceProcessInstaller processInstaller = new ServiceProcessInstaller();
+        ServiceInstaller serviceInstaller = new ServiceInstaller();
+
+        processInstaller.Account = ServiceAccount.LocalSystem;
+
+        serviceInstaller.ServiceName = "FileBackupService";
+        serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+        Installers.Add(processInstaller);
+        Installers.Add(serviceInstaller);
+    }
+}
